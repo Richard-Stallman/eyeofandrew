731645c608e69e26aaae2e269a82dd7d6a1d1e96
Matt Bryant <mbryant@andrew.cmu.edu>
2013-11-12 09:20:29 -0500

    Updated mongo driver, work on control

--- mongoDriver.py
+++ mongoDriver.py
@@ -1,9 +1,47 @@
 from pymongo import MongoClient
+from datetime import datetime
 
 def insert(data):
-    # Replace dots with dashes in the keys
-    mData = dict((k.replace(".","-"), v) for (k,v) in iter(data.items()))
+    mData = mongoFormat(data)
     dbclient = MongoClient()
-    db = dbclient["eyeofandrew"];
-    collection = db["all"]
-    collection.insert(mData)
+    dbclient["eyeofandrew"]["all"].insert(mData)
+
+def mongoFormat(data):
+    results = []
+    for host in data:
+        for date in data[host]:
+            for time in data[host][date]:
+                for index in data[host][date][time]:
+                    dt = datetime.strptime("{} {}".format(date, time), "%m/%d/%y %H:%M:%S")
+                    e = data[host][date][time][index]
+                    keys = {"host": host,
+                            "time": dt,
+                            "user": e["user"],
+                            "prog": e["prog"],
+                            "cpu": e["jcpu"],
+                            "tty": e["tty"],
+                            "from": e["from"],
+                            "login": e["login"],
+                            "idle": e["idle"]
+                           }
+                    results.append(keys)
+    return results
+
+class Query:
+    def __init__(self):
+        self.db = MongoClient()["eyeofandrew"]["all"]
+
+    def execute(self, *details):
+        queries = [getattr(self, name)(value) for (name, value) in details]
+        query = {k:v for q in queries for k,v in q.items()}
+        return self.db.find(query)
+
+    def machine(self, name):
+        return {"host": name}
+
+    def timeBetween(self, timeT):
+        (start, end) = timeT
+        return {"time": {"$gte": start, "$lt": end}}
+
+    def user(self, andrewid):
+        return {"user": andrewid}
